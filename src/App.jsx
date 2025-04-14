a1d65613a62db0cb69c2a4e08ba1ab5f5187b841
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2025-04-15 03:19:49 +0530

    Added code for fetching and displaying testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Testimonials from './components/Testimonials';
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Home />
+      <Testimonials />
     </>
   )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBrt-5z8S9AcsvezLxlR1Fiiw8sSgT_iJU",
@@ -14,5 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
-export {app, analytics};
+export {app, analytics, db};
